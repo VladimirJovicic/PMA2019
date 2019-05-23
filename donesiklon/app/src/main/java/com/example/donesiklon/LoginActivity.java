aff80dac264c449256930d7f4d8340037119dd72
Nikola Krajinovic <marko.krajinovic1233@gmail.com>
2019-05-23 19:13:10 +0200

    firebase added to project

--- donesiklon/app/src/main/java/com/example/donesiklon/LoginActivity.java
+++ donesiklon/app/src/main/java/com/example/donesiklon/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.donesiklon;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -10,6 +11,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
     Button signInButton;
